4f39a99b963d6a354e21e78ca09edaa620cdf2de
kong668836 <1013769643@qq.com>
2017-07-17 13:42:08 +0800

    Update city-picker.js

--- src/util/city-picker.js
+++ src/util/city-picker.js
@@ -1,299 +1,151 @@
-(function (wid, dcm) {
-    var win = wid;
-    var doc = dcm;
+import "whatwg-fetch";
+import vm from "../main.js";
 
-    function $id(id) {
-        return doc.getElementById(id);
-    }
+ // const _baseURL = 'http://10.254.254.46:31485';    //开发服务器，本地常用
+// const _baseURL = 'http://172.28.200.22:8190';     //测试服务器
+const _baseURL = 'http://104.199.226.50:8190';       //公网测试服务器
+// const _baseURL = 'http://www.tour.com:8190';
+// const _baseURL = 'http://www.app10.com:8190';
 
-    function $class(name) {
-        return doc.getElementsByClassName(name);
-    }
 
-    function loop(begin, length, fuc) {
-        for ( var i = begin; i < length; i++ ) {
-            if (fuc(i)) break;
-        }
-    }
+const doErr = (txt) => {
+	return (() => {
+		vm.$Modal.error({
+			title: "错误",
+			content: txt
+		})
+	})();
+}
+const getHeaders = (tk) => {
+	let _headers = {'Content-Type': 'application/json;charset=utf-8', "fr": "6"}
+	if (tk) {
+		const _tk = window.sessionStorage.getItem("token");
+		if (_tk) {
+			_headers['tk'] = _tk
+		} else {
+			vm.$Modal.remove();
+			setTimeout(() => {
+				vm.$Modal.warning({
+					title: "警告",
+					content: "您好，请先登陆",
+					onOk(){
+						vm.caller()
+					}
+				});
+			}, 320);
+			let err = new Error("请登录");
+			err.type = 110;
+			throw  err
+			// return
+		}
+	}
+	return _headers
+}
 
-    function on(action, selector, callback) {
-        doc.addEventListener(action, function (e) {
-            if (selector == e.target.tagName.toLowerCase() || selector == e.target.className || selector == e.target.id) {
-                callback(e);
-            }
-        })
-    }
+const httpConfig = (method, params, isTk) => {
+	if (method === "get") {
+		return {
+			method: "GET",
+			headers: getHeaders(isTk)
+		}
+	} else {
+		return {
+			method: "POST",
+			headers: getHeaders(isTk),
+			body: JSON.stringify(params)
+		}
+	}
+}
 
-    function MultiPicker(config) {
-        this.input     = config.input;
-        this.container = config.container;
-        this.jsonData  = config.jsonData;
-        this.success   = config.success;
+const http = (method, url, params, isTk, isSync) => {
+	if (method === "get") {
+		var _url = "";
+		for (let i in params) {
+			_url += `${i}=${params[i]}&`
+		}
+		if (_url.length) {
+			_url = "?" + _url.substring(0, _url.length - 1);
+		}
+		url = url + _url
+	}
+	return fetch(_baseURL + url, httpConfig(method, params, isTk)).then(res => {
+		if (res.ok) {
+			return res.json();
+		} else {
+			console.log(method + "请求失败", res.status, "请求地址" + url)
+			let msg = "";
+			switch ((res.status + "").charAt(0)) {
+				case "4":
+					msg = "客户端错误,错误代码:" + res.status;
+					break;
+				case "5":
+					msg = "服务器错误,错误代码:" + res.status;
+					break;
+				default:
+					msg = "未知错误,错误代码:" + res.status;
+			}
+			doErr(msg);
+		}
+	}).then(data => {
+		console.log(method + "请求,带入参数", params, "请求地址:", _baseURL + url);
+		if (data.code === 200) {
+			return data.data
+		} else {
+			console.log("!!!!错误", data);
+			let msg = data.msg;
+			if (!msg) {
+				switch (data.code) {
+					case 400:
+						msg = "请求参数错误";
+						break;
+					case 401:
+						msg = "登陆已过期，请重新登陆";
+						break;
+					case 403:
+						msg = "您的权限不够";
+						break;
+					case 404:
+						msg = "访问的资源不存在";
+						break;
+					case 405:
+						msg = "http请求方式错误";
+						break;
+					case 500:
+						msg = "服务器错误";
+						break;
+					default:
+						msg = "未知错误";
+				}
+			}
+			if (data.code == 401 && msg == "会话已失效，请重新登录") {
+				vm.login = false;
+				window.sessionStorage.clear();
+			}
+			if (isSync) {
+				setTimeout(() => {
+					doErr(msg);
+				}, 350)
+			} else {
+				doErr(msg);
+			}
+		}
+	}).catch(function (err) {
+		vm.$Modal.error({
+			title: "错误",
+			content: "获取数据失败，请刷新重试"
+		})
+		let error = new Error("获取数据失败");
+		error.type = 110;
+		throw error;
+	})
+};
 
-        this.ulCount   = 0; //记录上一次的
-        this.ulIdx     = 0; //ul下标计数器,前一次的计数器
-        this.ulDomArr  = [];//ul的dom元素,【a】
-        this.idxArr    = [];//更新后的ul的下标 【a】
-        this.jsonArr   = [];//用来存储每个ul的li中显示的arr【a】
-        this.liHeight  = 40;
-        this.maxHeight = [];//每个ul的最大高度【a】
-        this.distance  = [];//transition的移动位置【a】
-        this.start     = {
-            Y: 0,
-            time: ''
-        };
-        this.move      = {
-            Y: 0,
-            speed: []
-        };
-        this.end       = {
-            Y: 0,
-            status: true,
-        };
-        this.resultArr = [];
-        this.initDomFuc();
-        this.initReady(0, this.jsonData[0]);
-        this.initBinding();
-    }
-
-    MultiPicker.prototype = {
-        constructor: MultiPicker,
-        generateArrData: function (targetArr) {
-            var tempArr = [];
-            loop(0, targetArr.length, function (i) {
-                tempArr.push({
-                    "id": targetArr[i].id,
-                    "value": targetArr[i].value
-                })
-            });
-            return tempArr;
-        },
-        checkArrDeep: function (parent) {
-            var _this = this;
-            if ('child' in parent && parent.child.length > 0) {
-                _this.jsonArr.push(_this.generateArrData(parent.child));
-                _this.checkArrDeep(parent.child[0]);
-            }
-            _this.idxArr.push(this.ulIdx++);
-        },
-        insertLiArr: function (targetUl, arr) {
-            var html    = '';
-            var nullObj = {
-                id: '-99',
-                value: '',
-            };
-            arr.unshift(nullObj, nullObj);
-            arr.push(nullObj, nullObj);
-            loop(0, arr.length, function (i) {
-                html += '<li data-id="' + arr[i].id + '">' + arr[i].value + '</li>';
-            });
-            targetUl.innerHTML = html;
-        },
-        initDomFuc: function () {
-            var _this                      = this;
-            var html                       = '';
-            html += '<div class="multi-picker-bg" id="multi-picker-bg-' + _this.container + '">'
-            + '<div  class="multi-picker-container" id="multi-picker-container-' + _this.container + '">'
-            + '<div class="multi-picker-btn-box">'
-            + '<div class="multi-picker-btn" id="multi-picker-btn-cancel">返回</div>'
-            + '<div class="multi-picker-btn" id="multi-picker-btn-save-' + _this.container + '">提交</div>'
-            + '</div>'
-            + '<div class="multi-picker-content">'
-            + '<div class="multi-picker-up-shadow"></div>'
-            + '<div class="multi-picker-down-shadow"></div>'
-            + '<div class="multi-picker-line"></div>'
-            + '</div></div></div>';
-            $id(_this.container).innerHTML = html;
-            _this.jsonArr.push(_this.generateArrData(_this.jsonData));
-        },
-        initReady: function (idx, target) {
-            var _this            = this;
-            this.ulIdx           = 0;
-            this.idxArr.length   = idx;
-            _this.jsonArr.length = idx + 1;
-            _this.checkArrDeep(target);
-            var parentNode = $id('multi-picker-container-' + _this.container).children[1];
-            var tempMax    = _this.ulCount <= _this.idxArr.length ? _this.ulCount : _this.idxArr.length;
-            loop(idx + 1, tempMax, function (i) {
-                var $picker = $id('multi-picker-' + _this.container + '-' + i);
-                _this.insertLiArr($picker, _this.jsonArr[i]);
-                _this.distance[i]             = 0;
-                $picker.style.transform       = 'translate3d(0, 0, 0)';
-                $picker.style.webkitTransform = 'translate3d(0, 0, 0)';
-            });
-            if (_this.ulCount <= _this.idxArr.length) {
-                loop(_this.ulCount, _this.idxArr.length, function (i) {
-                    var newPickerDiv = document.createElement('div');
-                    newPickerDiv.setAttribute('class', 'multi-picker');
-                    newPickerDiv.innerHTML = '<ul id="multi-picker-' + _this.container + '-' + i + '"></ul>';
-                    parentNode.insertBefore(newPickerDiv, parentNode.children[parentNode.children.length - 3]);
-                    var tempDomUl = $id('multi-picker-' + _this.container + '-' + i);
-                    _this.ulDomArr.push(tempDomUl);
-                    _this.distance.push(0);
-
-                    _this.insertLiArr(tempDomUl, _this.jsonArr[i]);
-
-                    var tempArray = _this.jsonArr[i];
-                    tempDomUl.addEventListener('touchstart', function () {
-                        _this.touch(event, _this, tempDomUl, tempArray, i);
-                    }, false);
-                    tempDomUl.addEventListener('touchmove', function () {
-                        _this.touch(event, _this, tempDomUl, tempArray, i);
-                    }, false);
-                    tempDomUl.addEventListener('touchend', function () {
-                        _this.touch(event, _this, tempDomUl, tempArray, i);
-                    }, true);
-                });
-            } else {
-                for ( var j = _this.ulCount - 1; j > _this.idxArr.length - 1; j-- ) {
-                    var oldPicker = $id(_this.container).querySelectorAll('.multi-picker')[j];
-                    oldPicker.parentNode.removeChild(oldPicker);
-                    _this.ulDomArr.pop();
-                    _this.distance.pop();
-                }
-            }
-
-            _this.maxHeight.length = 0;
-            _this.resultArr.length = 0;
-            loop(0, _this.idxArr.length, function (i) {
-                $id(_this.container).querySelectorAll('.multi-picker')[i].style.width = 100 / _this.idxArr.length + '%';
-                _this.maxHeight.push($id('multi-picker-' + _this.container + '-' + i).childNodes.length * _this.liHeight);
-                _this.resultArr.push({
-                    "id": _this.jsonArr[i][_this.distance[i] / _this.liHeight + 2].id,
-                    "value": _this.jsonArr[i][_this.distance[i] / _this.liHeight + 2].value,
-                });
-            });
-            _this.ulCount = _this.idxArr.length;
-        },
-        initBinding: function () {
-            var _this     = this;
-            var bg        = $id('multi-picker-bg-' + _this.container);
-            var container = $id('multi-picker-container-' + _this.container);
-            var body      = doc.body;
-            on('touchstart', _this.input, function () {
-                bg.classList.add('multi-picker-bg-up');
-                container.classList.add('multi-picker-container-up');
-                body.classList.add('multi-picker-locked');
-            }, false);
-
-            on('touchstart', 'multi-picker-btn-save-' + _this.container, function () {
-                _this.success(_this.resultArr);
-                bg.classList.remove('multi-picker-bg-up');
-                container.classList.remove('multi-picker-container-up');
-                body.classList.remove('multi-picker-locked');
-            }, false);
-
-            on('touchstart', 'multi-picker-bg-' + _this.container, function () {
-                bg.classList.remove('multi-picker-bg-up');
-                container.classList.remove('multi-picker-container-up');
-                body.classList.remove('multi-picker-locked');
-            }, false);
-
-            on('touchstart', 'multi-picker-btn-cancel', function () {
-                bg.classList.remove('multi-picker-bg-up');
-                container.classList.remove('multi-picker-container-up');
-                body.classList.remove('multi-picker-locked');
-            }, false);
-        },
-        checkRange: function (idx) {
-            var _this     = this;
-            var tempObj   = _this.jsonData;
-            var targetIdx = 0;
-            loop(0, idx + 1, function (i) {
-                targetIdx = _this.distance[i] / _this.liHeight;
-                tempObj   = i == 0 ? tempObj[targetIdx] : tempObj.child[targetIdx];
-            });
-            _this.initReady(idx, tempObj);
-        },
-        initPosition: function (dis, max, idx) {
-            dis     = dis < 0 ? 0 : dis;
-            dis     = dis > max ? max : dis;
-            var sub = dis % this.liHeight;
-            if (sub < this.liHeight / 2) {
-                this.distance[idx] = dis - sub;
-            } else {
-                this.distance[idx] = dis + (this.liHeight - sub);
-            }
-            return this;
-        },
-        initSpeed: function (arr, dir, max, idx) {
-            var variance = 0;
-            var sum      = 0;
-            var rate     = 0;
-            for ( var i in arr ) {
-                sum += arr[i] - 0;
-            }
-            for ( var j in arr ) {
-                variance += (arr[j] - (sum / arr.length)) * (arr[j] - (sum / arr.length));
-            }
-            if ((variance / arr.length).toFixed(2) > .1) {
-                rate = max > this.liHeight * 15 ? dir * 2 : 0;
-                this.initPosition(this.distance[idx] + rate, max - this.liHeight * 5, idx);
-                this.move.speed[0] = .2;
-            } else {
-                this.initPosition(this.distance[idx], max, idx);
-                this.move.speed[0] = this.move.speed[0] > 0.2 ? .2 : this.move.speed[0];
-            }
-        },
-        touch: function (event, that, $picker, array, idx) {
-            event = event || window.event;
-            event.preventDefault();
-            switch (event.type) {
-                case "touchstart":
-                    if (that.end.status) {
-                        that.end.status = !that.end.status;
-                        event.preventDefault();
-                        that.move.speed = [];
-                        that.start.Y    = event.touches[0].clientY;
-                        that.start.time = Date.now();
-                    }
-                break;
-                case "touchend":
-                    that.end.Y         = Math.abs(event.changedTouches[0].clientY);
-                    var tempDis        = that.distance[idx] + (that.start.Y - that.end.Y);
-                    var temp           = that.distance[idx];
-                    that.distance[idx] = tempDis < 0 ? 0 : (tempDis < that.maxHeight[idx] - this.liHeight * 5 ? tempDis : that.maxHeight[idx] - this.liHeight * 5);
-                    that.initSpeed(that.move.speed, that.start.Y - that.end.Y, that.maxHeight[idx], idx);
-
-                    $picker.style.transform        = 'translate3d(0,-' + that.distance[idx] + 'px, 0)';
-                    $picker.style.webkitTransform  = 'translate3d(0,-' + that.distance[idx] + 'px, 0)';
-                    $picker.style.transition       = 'transform ' + that.move.speed[0] + 's ease-out';
-                    $picker.style.webkitTransition = '-webkit-transform ' + that.move.speed[0] + 's ease-out';
-                    //设置后续ul;
-                    if (temp != that.distance[idx]) that.checkRange(idx);
-                    setTimeout(function () {
-                        that.end.status = true;
-                    }, that.move.speed[0] * 1000);
-                break;
-                case "touchmove":
-                    event.preventDefault();
-                    that.move.Y = event.touches[0].clientY;
-                    var offset  = that.start.Y - that.move.Y;
-                    if (that.distance[idx] == 0 && offset < 0) {
-                        $picker.style.transform        = 'translate3d(0,' + 1.5 * that.liHeight + 'px, 0)';
-                        $picker.style.webkitTransform  = 'translate3d(0,' + 1.5 * that.liHeight + 'px, 0)';
-                        $picker.style.transition       = 'transform 0.2s ease-out';
-                        $picker.style.webkitTransition = '-webkit-transform 0.2s ease-out';
-                    } else {
-                        $picker.style.transform       = 'translate3d(0,-' + (offset + that.distance[idx]) + 'px, 0)';
-                        $picker.style.webkitTransform = 'translate3d(0,-' + (offset + that.distance[idx]) + 'px, 0)';
-                    }
-                    if (Math.abs(offset).toFixed(0) % 5 === 0) {
-                        var time = Date.now();
-                        that.move.speed.push((Math.abs(offset) / (time - that.start.time)).toFixed(2));
-                    }
-                    break;
-            }
-        }
-    };
-
-    if (typeof exports == "object") {
-        module.exports = MultiPicker;
-    } else if (typeof define == "function" && define.amd) {
-        define([], function () {
-            return MultiPicker
-        })
-    } else {
-        win.MultiPicker = MultiPicker;
-    } 
-})(window, document);
+export const get = (url, params = {}, isTk = false, isSync = false) => {
+	return http("get", url, params, isTk, isSync)
+}
+export const post = (url, params = {}, isTk = false, isSync = false) => {
+	return http("post", url, params, isTk, isSync)
+}
+export const baseURL = function () {
+	return _baseURL
+}
